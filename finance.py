1f6011470a7a0da0bc072013349f4f2a3350abd4
antoine-he <antoine.he@pm.me>
2018-05-06 20:03:15 -0400

    Codeio link

--- finance.py
+++ finance.py
@@ -9,12 +9,13 @@ def collect_market(stock, years=10):
     param['i'] = '86400'
     param['p'] = str(years) + 'Y'
     df = get_price_data(param)
-    df['date'] = df.index.map(lambda x: x.strftime('%Y-%m'))
-    df = df.groupby('date').mean()
+    #df['date'] = df.index.map(lambda x: x.strftime('%Y-%m'))
+    #df = df.groupby('date').mean()
     return df.loc[:, df.columns != 'Volume']    
 
 # Technology market
 tech_prices = collect_market('NDAQ')
+print(tech_prices, 2)
 
 # Renewable energy market
 energy_prices = collect_market('OSPTX')
@@ -27,4 +28,3 @@ oil_prices = collect_market('CL1')
 
 # Gold market
 gold_prices = collect_market('HUI')
-print(gold_prices)
