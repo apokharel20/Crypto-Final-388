4a01f5f328be1e07248b26d9ee8767b2bce7d195
antoine-he <antoine.he@pm.me>
2018-05-07 20:23:03 -0400

    Flux

--- finance.py
+++ finance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from googlefinance.client import get_price_data
+import numpy as np
 
 # Collect prices per month from a given market
 def collect_market(stock, years=10):
@@ -13,18 +14,21 @@ def collect_market(stock, years=10):
     #df = df.groupby('date').mean()
     return df.loc[:, df.columns != 'Volume']    
 
+def flux(stock, years):
+    df = collect_market(stock, years)['Close'].tolist()
+    return np.array([100 * (x - df[i - 1])/df[i - 1] for i, x in enumerate(df)][1:])
+
 # Technology market
-tech_prices = collect_market('NDAQ')
-print(tech_prices, 2)
+#tech_prices = collect_market('NDAQ')
 
 # Renewable energy market
-energy_prices = collect_market('OSPTX')
+#energy_prices = collect_market('OSPTX')
 
 # Real state market
-state_prices = collect_market('DJUSRE')
+#state_prices = collect_market('DJUSRE')
 
 # Oil market
-oil_prices = collect_market('CL1')
+#oil_prices = collect_market('CL1')
 
 # Gold market
-gold_prices = collect_market('HUI')
+#gold_prices = collect_market('HUI')
